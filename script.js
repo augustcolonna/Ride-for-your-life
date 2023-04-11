8d5908631c3715cec92e417a4da942aef0ce812e
Gus <august.colonna@gmail.com>
2023-04-11 16:58:25 +0200

    game logic is complete, a couple bugs to squash, need to add audio and then done

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ window.onload = () => {
     //game states
     const startScreen = document.querySelector('.start-game');
     const startBtn = document.querySelector('#start-button');
+    const restartBtn = document.querySelector('#restart')
+    //const homeBtn = document.querySelector('#home-screen')
     const endScreen = document.querySelector('#game-over');
     endScreen.style.display = 'none' 
     
@@ -18,6 +20,9 @@ window.onload = () => {
     const bgImg = new Image()
     bgImg.src = './Images/background2.jpg'
 
+    //let bg1x = 0;
+    //let bg2x = -canvas-width;
+    
     const player = new Image()
     player.src = './Images/bicycle.png'
     
@@ -29,6 +34,8 @@ window.onload = () => {
     const boulder = new Image()
     boulder.src = './Images/stone.png'
 
+    const negativeImgArr = ['./Images/hiker.png','./Images/pine-tree.png','./Images/stone.png']
+
     //posisitve objects
     const water = new Image()
     water.src = './Images/plastic-bottle.png'
@@ -37,6 +44,9 @@ window.onload = () => {
     const airPump = new Image()
     airPump.src = './Images/pump.png'
 
+    const positiveImgArr = ['./Images/chocolate.png', './Images/pump.png', './Images/plastic-bottle.png']
+
+
     //player movement variables
     let isMovingLeft = false;
     let isMovingRight = false;
@@ -44,54 +54,50 @@ window.onload = () => {
     let isMovingDown = false;
     let playerX = 80;
     let playerY = 300;
-    let playerWidth = 60;
-    let playerHeight = 60;
+    let playerWidth = 70;
+    let playerHeight = 70;
     let playerSpeed = 4;
 
-
     //game misc. variables
-    let gameOver = false;
     let animateId = 0;
 
-     //score
-     let yourScore = 0;
-     let lives = 4;
+    //score
+    let yourScore = 0;
+    let lives = 4;
 
     //+ obstacle classes, functions
-     const positiveImgArr = ['./Images/chocolate.png', './Images/pump.png', './Images/plastic-bottle.png']
-
     class Obstacle {
-        constructor(width, height, x, y, speed) {
-            this.width = 30;
-            this.height = 50;
-            this.x = 1400 - this.width;
+        constructor() {
+            this.width = 70;
+            this.height = 80;
+            this.x = 1000 - this.width;
             this.y = Math.random()*(canvas.height-this.height);
-            this.speed = 2;
+            this.speed = 1.5;
             this.img = airPump
-        }
+        };
         drawObstacle() {
             ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-        }
+        };
         update(){
             this.x -= this.speed
-        }
-      }
+        };
+    };
 
       let positiveArr = [];
       const addPump = () => {
         const posObst = new Obstacle();
         positiveArr.push(posObst)
-      }
+    };
       const drawPump = () => {
         positiveArr.forEach((element) => {
             element.drawObstacle();
         });
-      };
+    };
       const updatePump = () => {
         positiveArr.forEach((element) => {
             element.update();
         });
-      };
+    };
 
       function posObstCollision (){
         positiveArr.forEach((element) => {
@@ -99,49 +105,53 @@ window.onload = () => {
                 playerX + playerWidth > element.x &&
                 playerY < element.y + element.height &&
                 playerY + playerHeight > element.y){
+                    
+                    element.x = 2000
+
                     yourScore += 1
                     
                     scoreAmount.innerText = 'Your Score:' + yourScore
-                }
-          });
-        }
+
+                    console.log("score increased by 1")
+                };
+        });
+    };
 
     //- obstacle classes, functions
-    const negativeImgArr = []
 
     class Obstacle2 {
-        constructor(width, height, x, y, speed) {
-            this.width = 50;
-            this.height = 70;
-            this.x = 1400 - this.width;
+        constructor() {
+            this.width = 70;
+            this.height = 80;
+            this.x = 1000 - this.width;
             this.y = Math.random()*(canvas.height-this.height);
-            this.speed = 2;
+            this.speed = 1.5;
             this.img = hiker;
-        }
+        };
         drawObstacle2() {
             ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-        }
+        };
         update2(){
             this.x -= this.speed
 
-        }
-      }
+        };
+    };
 
       let negativeArr = [];
       const addHiker = () => {
         const negObst = new Obstacle2();
         negativeArr.push(negObst)
-      }
+    };
       const drawHiker = () => {
         negativeArr.forEach((element) => {
             element.drawObstacle2();
         });
-      };
+    };
       const updateHiker = () => {
         negativeArr.forEach((element) => {
             element.update2();
         });
-      };
+    };
 
       function negObstCollision () {
         negativeArr.forEach((element) => {
@@ -149,49 +159,75 @@ window.onload = () => {
                 playerX + playerWidth > element.x &&
                 playerY < element.y + element.height &&
                 playerY + playerHeight > element.y){
+                    
+                    element.x = 2000
+                    
                     lives -= 1
                     
                     lifeAmount.innerText = 'Lives left:' + lives
-                }
-          });
-        }
+
+                    console.log("lost a life")
+                };
+        });
+    };
+
+        function drawPlayer () {
+            ctx.drawImage(player, playerX, playerY, playerWidth, playerHeight)
+    };
 
     //animation
     const animate = () => {
+    /*//moving background 1
+    ctx.drawImage(bgImg, 0, bg1x, canvas.width, canvas.height);
+    //moving background 2
+    ctx.drawImage(bgImg, 0, bg2x, canvas.width, canvas.height);
+
+    //make them move
+    bg1x += playerSpeed
+    bg2x += playerSpeed
+
+    //check for end of backround
+    if(bg1x > canvas.width){
+        bg1x = -canvas.width;
+    }
+    if(bg2x > canvas.width){
+        bg2x = -canvas.width;
+    }*/
     ctx.drawImage(bgImg, 0,0, canvas.width, canvas.height)
-    ctx.drawImage(player, playerX, playerY, playerWidth, playerHeight)
-    
-    if(isMovingLeft && playerX > 0){
-        playerX -= playerSpeed
-    }
-    else if(isMovingRight && playerX < canvas.width - playerWidth){
-        playerX += playerSpeed
-    }
-    else if(isMovingDown && playerY < canvas.height - playerHeight){
-        playerY += playerSpeed
-    }
-    else if(isMovingUp && playerY > 0){
-        playerY -= playerSpeed
-    }
+    drawPlayer()
+        
+        if(isMovingLeft && playerX > 0){
+            playerX -= playerSpeed
+        }
+        else if(isMovingRight && playerX < canvas.width - playerWidth){
+            playerX += playerSpeed
+        }
+        else if(isMovingDown && playerY < canvas.height - playerHeight){
+            playerY += playerSpeed
+        }
+        else if(isMovingUp && playerY > 0){
+            playerY -= playerSpeed
+            };
 
-    if(animateId % 100 === 0){
-       addPump(); 
-       addHiker();
-       posObstCollision();
-       negObstCollision();
-    }
-    drawPump();
-    drawHiker();
-    updatePump();
-    updateHiker();
-    
-    animateId = requestAnimationFrame(animate)
+        if(animateId % 100 === 0){
+        addPump(); 
+        addHiker();
+        posObstCollision();
+        negObstCollision();
+        }
+            drawPump();
+            drawHiker();
+            updatePump();
+            updateHiker();
 
-    if(lives === 0){
-        cancelAnimationFrame(animateId)
-        console.log("game over")
+        if(lives === 0){
+            cancelAnimationFrame(animateId)
+            gameIsOver()
+            console.log("game over")
+        }else{
+            animateId = requestAnimationFrame(animate)
+        };
     };
-}
 
     function startGame () {
         console.log('Put that helmet on, its going to get bumpy')
@@ -200,12 +236,79 @@ window.onload = () => {
         canvas.style.display = 'block';
         scoreAmount.style.display = 'block';
         lifeAmount.style.display = 'block';
+
         animate()
-    }
+
+    };
+
+    function gameIsOver () {
+        const totalScore = document.querySelector('.score');
+        totalScore.innerHTML = 'Final Score:' + yourScore;
+        startScreen.style.display = 'none';
+        canvas.style.display = 'none';
+        scoreAmount.style.display = 'none';
+        lifeAmount.style.display = 'none';
+        endScreen.style.display = 'block';
+        restartBtn.style.display = 'block';
+
+        
+    };
+
+    /*homeBtn.addEventListener('click', () => {
+        startScreen.style.display = 'block'
+        canvas.style.display = 'none';
+        scoreAmount.style.display = 'none';
+        lifeAmount.style.display = 'none';
+        endScreen.style.display = 'none';
+
+        isMovingLeft = false;
+        isMovingRight = false;
+        isMovingUp = false;
+        isMovingDown = false;
+        playerX = 80;
+        playerY = 300;
+        playerWidth = 70;
+        playerHeight = 70;
+        playerSpeed = 4;
+
+       //game misc. variables
+        animateId = 0;
+
+       //score
+        yourScore = 0;
+        lives = 4;
+    });*/
 
     startBtn.addEventListener('click', startGame)
 
+    restartBtn.addEventListener('click', () => {
+        restartBtn.style.display = 'none';
+        startScreen.style.display = 'none'
+        canvas.style.display = 'block';
+        scoreAmount.style.display = 'block';
+        lifeAmount.style.display = 'block';
+        endScreen.style.display = 'none';
+       
+         isMovingLeft = false;
+         isMovingRight = false;
+         isMovingUp = false;
+         isMovingDown = false;
+         playerX = 80;
+         playerY = 300;
+         playerWidth = 70;
+         playerHeight = 70;
+         playerSpeed = 4;
 
+        //game misc. variables
+         
+         animateId = 0;
+
+        //score
+         yourScore = 0;
+         lives = 4;
+
+        startGame()
+    });
 
     document.addEventListener('keydown', event => {
         if(event.key === 'a' || event.key === 'A'){
@@ -220,7 +323,7 @@ window.onload = () => {
         if(event.key === 's' || event.key === 'S'){
             isMovingDown = true;
         }
-    })
+    });
 
     document.addEventListener('keyup', event => {
         if(event.key === 'a' || event.key === 'A'){
@@ -235,5 +338,8 @@ window.onload = () => {
         if(event.key === 's' || event.key === 'S'){
             isMovingDown = false;
         } 
-})
+    });
+
 }
+
+
